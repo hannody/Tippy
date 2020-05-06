0230b6d8d8cf0fab08aedc9c4bd0773bf05fb0a9
hannody <aljaboori@gmail.com>
2020-05-06 10:12:22 +0800

    [UPDATE] WIP: Seekbar implementation

--- app/src/main/java/com/abunayla/tippy/MainActivity.kt
+++ app/src/main/java/com/abunayla/tippy/MainActivity.kt
@@ -2,11 +2,30 @@ package com.abunayla.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        seekBar_tipPercent.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG,"OnProgressChanged $progress")
+                textView_percent.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
