809c01d1c987d263670e0ffa8cb9d76f8087f15a
hannody <aljaboori@gmail.com>
2020-05-11 19:32:12 +0800

    [UPDATE] Emoji animation, URL to my website. [NEXT] 1. some UI Design improvement 2. Per pax total division.

--- app/src/main/java/com/abunayla/tippy/MainActivity.kt
+++ app/src/main/java/com/abunayla/tippy/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.abunayla.tippy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableString
+import android.text.Spanned
 import android.text.TextWatcher
-import android.util.Log
+import android.text.style.ClickableSpan
+import android.text.style.URLSpan
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 private const val TAG = "MainActivity"
+
 private const val INIT_PERCENT_VALUE = 15
+
+
+
 class MainActivity : AppCompatActivity() {
 
 
@@ -20,23 +28,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         seekBar_tipPercent.progress = INIT_PERCENT_VALUE
         textView_percent.text = "$INIT_PERCENT_VALUE%"
+        updateEmoji(INIT_PERCENT_VALUE)
+
+
 
         seekBar_tipPercent.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                Log.i(TAG,"OnProgressChanged $progress")
+                //Log.i(TAG,"OnProgressChanged $progress")
+
+
                 textView_percent.text = "$progress%"
                 computeTipAndTotal()
+
+                updateEmoji(progress)
 
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if(seekBar_tipPercent.progress < 2) {
+                    seekBar_tipPercent.progress = 1
+                }
+            }
         })// seekBar_tipPercent.setOnSeekBarChangeListener
 
         editText_baseAmount.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                Log.i(TAG, "afterTextChanged $s")
+                //Log.i(TAG, "afterTextChanged $s")
                 computeTipAndTotal()
 
             }
@@ -48,6 +67,17 @@ class MainActivity : AppCompatActivity() {
 
     }//onCreate
 
+    private fun updateEmoji(tipPercent: Int) {
+        val emojis = mutableListOf("\uD83D\uDE2D", "☹️", "\uD83D\uDE07","\uD83E\uDD70", "\uD83E\uDD11")
+        when(tipPercent){
+            in 0..3-> textView_emoji.text = emojis.get(0)
+            in 4..10-> textView_emoji.text = emojis.get(1)
+            in 11..20-> textView_emoji.text = emojis.get(2)
+            in 21..25-> textView_emoji.text = emojis.get(3)
+            else-> textView_emoji.text = emojis.get(4)
+        }
+    }
+
     private fun computeTipAndTotal() {
         if (editText_baseAmount.text.isEmpty()){
             textView_tipAmount.text =""
@@ -55,13 +85,16 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val baseAmount = editText_baseAmount.text.toString().toDouble()
-        val tipPercent = seekBar_tipPercent.progress
-        val tipAmount = (baseAmount * tipPercent)/100
-        val totalAmount = (baseAmount + tipAmount)
+        if(seekBar_tipPercent.progress > 0) {
 
-        textView_tipAmount.text = "%.2f".format(tipAmount)
-        textView_totalAmount.text = "%.2f".format(totalAmount)
+            val baseAmount = editText_baseAmount.text.toString().toDouble()
+            val tipPercent = seekBar_tipPercent.progress
+            val tipAmount = (baseAmount * tipPercent) / 100
+            val totalAmount = (baseAmount + tipAmount)
+
+            textView_tipAmount.text = "%.2f".format(tipAmount)
+            textView_totalAmount.text = "%.2f".format(totalAmount)
+        }
 
     }
 }
